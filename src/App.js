116f856aeba20125fe579c4ff5578b7d7b1777b3
Daryna <krukds2019@gmail.com>
2023-12-05 22:22:37 +0200

    Revert "[feat] krukdaryna add personal info"
    This reverts commit b103dcbb2c706eb13c3c0af1ac20cc781f08e4d0.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import daryna from './images/daryna.jpg'
+// import photo from './images...'
 
 function App() {
   return (
     <div className="App">
       <h1>Our Team</h1>
       <div className="team">
-          <div className='participant-info'>
-              <text className='name'>Kruk Daryna</text>
-              <text className='job'>Copyright & Tester</text>
-              <img className='participant-photo' src={daryna} alt="" />
-          </div>
+        {/* <div className='participant-info'>
+          <text className='name'>Your name</text>
+          <text className='job'>Your job</text>
+          <img className='participant-photo' src={photo} alt="" />
+        </div> */}
       </div>
     </div>
   );
