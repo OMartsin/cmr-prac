0595f9653da6fe8a41aaaa879681b0dac8d1a1a1
OMartsin <oleksandrmartsin@gmail.com>
2023-12-05 22:02:27 +0200

    [feat] add martsin oleksandr info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-// import photo from './images...'
+import oleksandr from './images/oleksandr.png';
 
 function App() {
   return (
     <div className="App">
       <h1>Our Team</h1>
       <div className="team">
-        {/* <div className='participant-info'>
-          <text className='name'>Your name</text>
-          <text className='job'>Your job</text>
-          <img className='participant-photo' src={photo} alt="" />
-        </div> */}
+        <div className='participant-info'>
+          <text className='name'>Oleksandr Martsin</text>
+          <text className='job'>Projeck Manager</text>
+          <img className='participant-photo' src={oleksandr} alt="" />
+        </div>
       </div>
     </div>
   );
