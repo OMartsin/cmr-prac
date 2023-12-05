c94b1c29e603ef7ac8e7856aa7141874a6eeee21
Andriy-Symon <drinlviv@gmail.com>
2023-12-05 21:53:28 +0200

    [feat] drin add personal info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-// import photo from './images...'
+import andriyPhoto from './images/andriy.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Our Team</h1>
       <div className="team">
-        {/* <div className='participant-info'>
-          <text className='name'>Your name</text>
-          <text className='job'>Your job</text>
-          <img className='participant-photo' src={photo} alt="" />
-        </div> */}
+        {
+          <div className='participant-info'>
+          <text className='name'>Andriy-Symon Herman</text>
+          <text className='job'>Developer</text>
+          <img className='participant-photo' src={andriyPhoto} alt="Andriy" />
+        </div>
+        }
       </div>
     </div>
   );
