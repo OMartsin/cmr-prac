ad98d1de9d48878496bea7c0acca95becf40a5ca
Daryna <krukds2019@gmail.com>
2023-12-05 22:44:35 +0200

    [feat] krukdaryna add personal info

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-// import photo from './images...'
+import daryna from './images/daryna.jpg'
 
 function App() {
   return (
     <div className="App">
       <h1>Our Team</h1>
-      <div className="team">
-        {/* <div className='participant-info'>
-          <text className='name'>Your name</text>
-          <text className='job'>Your job</text>
-          <img className='participant-photo' src={photo} alt="" />
-        </div> */}
-      </div>
+        <div className="team">
+            <div className='participant-info'>
+                <text className='name'>Kruk Daryna</text>
+                <text className='job'>Copyright & Tester</text>
+                <img className='participant-photo' src={daryna} alt="avatar" />
+            </div>
+        </div>
     </div>
   );
 }
