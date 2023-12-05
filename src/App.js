84dd0ff5a0c54df3af34a9a6060b40a46dc528ee
Alina <alnmrtnk>
2023-12-05 21:37:45 +0200

    [feat] alnmrtnk add personal info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-// import photo from './images...'
+import alina from './images/alina.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Our Team</h1>
       <div className="team">
-        {/* <div className='participant-info'>
-          <text className='name'>Your name</text>
-          <text className='job'>Your job</text>
-          <img className='participant-photo' src={photo} alt="" />
-        </div> */}
+        <div className='participant-info'>
+          <text className='name'>Alina Martyniak</text>
+          <text className='job'>UI/UX Designer</text>
+          <img className='participant-photo' src={alina} alt="" />
+        </div>
       </div>
     </div>
   );
